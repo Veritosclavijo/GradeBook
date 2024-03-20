8b471fce2fa1ce095e4c7433687413d27f7c33aa
Veritosclavijo <veronica.clavijo1411@ucaldas.edu.co>
2024-03-20 17:34:01 -0500

    first commit

--- src/gradebook/GradeBook.java
+++ src/gradebook/GradeBook.java
@@ -1,2 +1,21 @@
-package gradebook;public class gradebook {
+package gradebook;
+
+public class GradeBook {
+
+    private String courseName;
+
+    public GradeBook(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    public String displayMessage(){
+        return getCourseName();
+    }
 }
--- src/gradebook/GradebookTest.java
+++ src/gradebook/GradebookTest.java
@@ -1,5 +1,14 @@
-public class gradebookTest {
+package gradebook;
+
+public class GradebookTest {
     public static void main(String[] args) {
+
+        GradeBook gradeBook1 = new GradeBook("CS101 Introduction to Java Programming");
+        GradeBook gradeBook2 = new GradeBook("CS102 Data Structures in Java");
+
+        System.out.println("gradeBook1 course name is: " + gradeBook1.displayMessage());
+        System.out.println("gradeBook2 course name is: " + gradeBook2.displayMessage());
+
 
     }
 }
